4017aa44eb87c387164fca787a4ce378469f6f4a
Md Rijwan Razzaq Matin <mrrmmonty@gmail.com>
2023-10-13 04:52:01 +0800

    chore: adding data models to be exported

---
+++ src/helpers/model.ts
@@ -0,0 +1,22 @@
+export type s3DataModel = {
+  message: string
+  owner: string
+  numberOfFiles: number
+  files: fileInfoModel[]
+}
+
+export type fileInfoModel = {
+  fileName: string
+  fileType: string
+  fileSize: string
+  entityTag: string
+  fileStatus: string
+  s3Uri: string
+}
+
+export type s3ParamsModel = {
+  Bucket: string
+  Key: string
+  Body: object
+  ContentType: string
+}
