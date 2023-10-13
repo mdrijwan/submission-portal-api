e28c2ce08bbf3e77952b89453869f954bbd98066
Md Rijwan Razzaq Matin <mrrmmonty@gmail.com>
2023-10-14 04:27:14 +0800

    feat: adding authorizer to the handler

--- src/api/submit.ts
+++ src/api/submit.ts
@@ -1,30 +1,56 @@
 import { parse } from 'lambda-multipart-parser'
 import { formatResponse, s3Upload } from '../helpers/util'
-import { StatusCode } from '../helpers/enums'
-import { fileInfoModel, s3DataModel, s3ParamsModel } from '../helpers/model'
+import { ErrorType, StatusCode } from '../helpers/enums'
+import {
+  fileInfoModel,
+  s3DataModel,
+  s3ParamsModel,
+  uploadInfoModel,
+  userInfoModel,
+} from '../helpers/model'
 import { APIGatewayProxyEvent } from 'aws-lambda'
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   try {
-    const username: string = event.headers.username
+    const identity = process.env.IS_OFFLINE
+      ? event.headers.email
+      : event.requestContext.authorizer.claims.email
+
+    const username = process.env.IS_OFFLINE
       ? event.headers.username
-      : ''
+      : event.requestContext.authorizer.claims.name
+
+    if (!identity) {
+      return formatResponse(StatusCode.ERROR, ErrorType.AUTH)
+    }
+
     const result = await parse(event)
-    const data: s3DataModel = {
-      message: 'Successfully uploaded files to S3',
-      owner: username,
-      numberOfFiles: result.files.length,
+
+    const userInfo: userInfoModel = {
+      user: username,
+      email: identity,
+    }
+
+    const uploadInfo: uploadInfoModel = {
+      filesUploaded: result.files.length,
       files: [],
     }
+
+    const data: s3DataModel = {
+      message: 'Successfully uploaded files to S3',
+      ...userInfo,
+      ...uploadInfo,
+    }
     // console.log('RESULT', result)
+    console.log('USER INFO', userInfo)
 
     for (let i = 0; i < result.files.length; i++) {
       const docFile = result.files[i]
       const docParam: s3ParamsModel = {
         Bucket: process.env.UPLOAD as string,
-        Key: `images/${username}/${docFile.fieldname}-${new Date()
-          .toISOString()
-          .replace('.', '-')}-${docFile.filename
+        Key: `images/${username.replace(/ /g, '-')}/${
+          docFile.fieldname
+        }-${new Date().toISOString().replace('.', '-')}-${docFile.filename
           .split(' ')
           .join('-')}`.toLowerCase(),
         Body: docFile.content as unknown as string,
@@ -48,6 +74,6 @@ export const handler = async (event: APIGatewayProxyEvent) => {
   } catch (error) {
     console.error(error)
 
-    return formatResponse(StatusCode.ERROR, error)
+    return formatResponse(StatusCode.ERROR, ErrorType.API)
   }
 }
