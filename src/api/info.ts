869c050f3919ebed3b3321fbe3feedac1332d08b
Md Rijwan Razzaq Matin <mrrmmonty@gmail.com>
2023-10-20 18:26:54 +0800

    feat: adding api to get user info

--- src/api/info.ts
+++ src/api/info.ts
@@ -1,5 +1,5 @@
 import { APIGatewayProxyEvent } from 'aws-lambda'
-import { formatResponse, getUploadData } from '../helpers/util'
+import { formatResponse, getUploadData, getUserData } from '../helpers/util'
 import { ErrorType, StatusCode } from '../helpers/enums'
 
 export const getUpload = async (event: APIGatewayProxyEvent) => {
@@ -16,6 +16,29 @@ export const getUpload = async (event: APIGatewayProxyEvent) => {
     console.log('USER ID', userId)
 
     const result = await getUploadData(uploadId, userId)
+    console.log('RESULT', result)
+
+    return formatResponse(StatusCode.SUCCESS, result)
+  } catch (error) {
+    console.error(error)
+
+    return formatResponse(StatusCode.ERROR, ErrorType.API)
+  }
+}
+
+export const getUser = async (event: APIGatewayProxyEvent) => {
+  try {
+    const userEmail = process.env.IS_OFFLINE
+      ? event.headers.email
+      : event.requestContext.authorizer.claims.email
+    const userId = process.env.IS_OFFLINE
+      ? event.pathParameters.userId
+      : event.requestContext.authorizer.claims.sub
+
+    console.log('EMAIL', userEmail)
+    console.log('USER ID', userId)
+
+    const result = await getUserData(userEmail)
     console.log('RESULT', result)
 
     return formatResponse(StatusCode.SUCCESS, result)
